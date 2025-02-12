e7333b4c463284434550a775243f9984c29ebb01
shakib-shajid <shakibalshajid1682@gmail.com>
2025-02-12 15:30:52 +0600

    responsive view added

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Banner from "./components/Banner";
 
 export default function Home() {
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="max-w-2xl lg:max-w-7xl mx-auto">
         <Banner />
     </div>
   );
